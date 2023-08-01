8b67f8749edd72eadd06cd16812013546003ef80
Sachin <sschin002@gmail.com>
2023-08-01 14:44:14 +0545

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="App">Hello World</div>;
+  return <></>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <ButtonComponent />
-    <Title />
   </React.StrictMode>
 );

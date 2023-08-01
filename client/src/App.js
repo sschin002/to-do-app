fc0bfd5ad2568ee7c08902cdd3702bc0f7e8f3ba
Sachin <sschin002@gmail.com>
2023-08-01 14:16:07 +0545

    project cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Update <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to Google
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;

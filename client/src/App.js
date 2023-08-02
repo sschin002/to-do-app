d9ac24c8a042f2f6cd46b43566010d7615c58b88
Sachin <sschin002@gmail.com>
2023-08-02 13:56:43 +0545

    logical && operator

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import Title from "./components/Title";
 import Example from "./components/Example";
+import Garage from "./components/Logical";
 function App() {
+  const cars = ["Ford", "BMW", "Audi", "Buggati"];
   return (
     <>
       <Title name="TODO APP" />
       <Example isGoal={false} />
+      <Garage cars={cars} />
     </>
   );
 }
---
+++ client/src/components/Logical.js
@@ -0,0 +1,11 @@
+function Logical(props) {
+  const cars = props.cars;
+  return (
+    <>
+      <h1>Garage</h1>
+      {cars.length > 0 && <h2>You have {cars.length} cars in your garage.</h2>}
+    </>
+  );
+}
+
+export default Logical;
